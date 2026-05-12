0f05b54a5483dc56d4d765d00e628c3625bfa929
PierreBiache <pbpierrebiache@gmail.com>
2026-05-12 14:40:30 +0200

    Update 02_⚙️_Research_Presentation.py

--- pages/02_⚙️_Research_Presentation.py
+++ pages/02_⚙️_Research_Presentation.py
@@ -4,10 +4,12 @@ from streamlit_pdf_viewer import pdf_viewer
 st.title(" Biomechanics & Ergonomics Research Master Project: Low-Tech Working Bed")
 
 st.markdown("""
-**Project Description:** This research project focuses on agricultural ergonomics to prevent Musculoskeletal Disorders (MSDs), which affect a vast majority of farmers. I analyzed the "Working Bed," a low-tech innovation designed to reduce severe lower back strain during ground-level farming tasks. 
-
-The primary objective was to determine the optimal seat adjustment based on the knee joint's torque-angle relationship to maximize force production capacity. By measuring muscle activity (EMG), task performance time, and perceived discomfort during a simulated harvesting task, this study highlights how biomechanical analysis and low-tech design can be combined to improve occupational health and daily efficiency.
-""")
+<div style="font-size: 1.2rem; line-height: 1.6;">
+    <strong>Project Description:</strong> This research project focuses on agricultural ergonomics to prevent Musculoskeletal Disorders (MSDs), which affect a vast majority of farmers. I analyzed the "Working Bed," a low-tech innovation designed to reduce severe lower back strain during ground-level farming tasks. 
+    <br><br>
+    The primary objective was to determine the optimal seat adjustment based on the knee joint's torque-angle relationship to maximize force production capacity. By measuring muscle activity (EMG), task performance time, and perceived discomfort during a simulated harvesting task, this study highlights how biomechanical analysis and low-tech design can be combined to improve occupational health and daily efficiency.
+</div>
+""", unsafe_allow_html=True)
 
 st.divider()
 
